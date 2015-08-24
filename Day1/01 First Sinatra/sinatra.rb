595b7b44c95986261d055c6b87d6a2d2b52a3a5a
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-08-24 11:47:11 +0200

    Started with the first Sinatra exercises

--- Day1/01 First Sinatra/sinatra.rb
+++ Day1/01 First Sinatra/sinatra.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'date'
 
 # set :bind, '0.0.0.0'
 
 get "/" do
-	erb :index
+	@current_time = Time.now.strftime("Today is %A, the %d of %B.")
+	erb :upload
+end
+
+post "/" do
+	tempfile = params[:file]
+	"Uploaded tempfile #{tempfile.inspect}"
 end
 
 post "/say_name" do
