07e97049296dd51deaf3ea413d3b0980790830b6
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-08-24 17:03:34 +0200

    Some progress on the second exercise

--- Day1/02 Exercise/main.rb
+++ Day1/02 Exercise/main.rb
@@ -6,19 +6,30 @@ phrases = ["Unpleasant nor diminution excellence apartments imprudence the met n
 					 "Draw part them he an to he roof only. Music leave say doors him. Tore bred form if sigh case as do.",
 					 "Staying he no looking if do opinion.",
 					 "Sentiments way understood end partiality and his."]
+list = nil
 
-get "/show" do
+get "/" do
 	@phrases = phrases
 	@random_phrase = @phrases[rand(0..(@phrases.count-1))]
+	@list = list
 	erb :index
 end
 
 get "/add" do
 	@phrases = phrases
 	@phrases.push(params[:new_phrase])
-	redirect to "/show"
+	redirect to "/"
 end
 
 get "/showall" do
-	@phrases = phrases
+	@list = phrases
+	@random_phrase = @list[rand(0..(@list.count-1))]
+	erb :index
 end
+
+post "/delete" do
+	@phrases = phrases
+	@phrases.delete(params[:delete_phrase])
+	redirect to "/showall"
+end
+
