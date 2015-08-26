c11a199403c785ee676a5ad1b06c201b9f80dd8c
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-08-26 14:23:33 +0200

    quickly did a fizz buzz TDD test

---
+++ Day3/03 FizzBuzz TDD/lib/fizzbuzz.rb
@@ -0,0 +1,13 @@
+class FizzBuzz
+	def run_game(number)
+		if number%15 == 0
+			return 'fizzbuzz'
+		elsif number%3 == 0
+			return 'fizz'
+		elsif number%5 == 0
+			return 'buzz'
+		else
+			return number
+		end
+	end
+end
---
+++ Day3/03 FizzBuzz TDD/specs/fizzbuzz_specs.rb
@@ -0,0 +1,22 @@
+require_relative '../lib/fizzbuzz'
+
+RSpec.describe FizzBuzz do
+  before do
+  	@game = FizzBuzz.new
+  end
+
+  describe "#game" do
+    it "replaces all multiples of 3 with 'fizz'" do
+      expect(@game.run_game(3)).to eq("fizz")
+    end
+    it "replaces all multiples of 5 with 'buzz'" do
+      expect(@game.run_game(5)).to eq("buzz")
+    end
+    it "replaces all multiples of 3 and 5 with 'fizzbuzz'" do
+      expect(@game.run_game(15)).to eq("fizzbuzz")
+    end
+    it "puts the number in all other cases" do
+      expect(@game.run_game(47)).to eq(47)
+    end
+  end
+end
