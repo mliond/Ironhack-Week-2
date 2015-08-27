b8feee1af7cd29b0e489d92f2524be33509902b3
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-08-27 15:17:51 +0200

    Started with Game of Life

---
+++ Day4/01 Game of Life/lib/cell.rb
@@ -0,0 +1,10 @@
+class Cell
+  def initialize(state, neighbours)
+    @state = state # either 1 or 0 for alive or dead
+    @neighbours = neighbours # an array that represents the surrounding cells
+  end
+
+  def regenerate
+    # your code here
+  end
+end
---
+++ Day4/01 Game of Life/specs/cell_specs.rb
@@ -0,0 +1,11 @@
+RSpec.describe Cell do
+	before :all do
+		@cell = Cell.new(1, [...])
+	end
+
+	describe "#method" do
+		it "should " do
+			expect(@cage).to eq(X)
+		end
+	end
+end
